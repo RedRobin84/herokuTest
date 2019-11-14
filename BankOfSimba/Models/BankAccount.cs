a0dbf94659c96067b1581a5b25e4620a5a1f4e69
Martin Cervenka <cervenka84@gmail.com>
2019-11-14 16:34:41 +0100

    heroku test

---
+++ BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        private double balance;
+        private string animalType;
+
+        public string Name { get; set; }
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        public string AnimalType
+        {
+            get { return animalType; }
+            set { animalType = value; }
+        }
+
+        public BankAccount(string name, double balance, string animalType)
+        {
+            this.Name = name;
+            this.Balance = balance;
+            this.AnimalType = animalType;
+        }
+        public string IsKing()
+        {
+            return this.Name.IndexOf("King") != -1 ? "Yes" : "No";
+        }
+
+
+
+
+
+    }
+}
